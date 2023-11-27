c3e9bb2685942c0a718268c4725cc5832412408f
anvo <songun1120@gamil.com>
2023-11-27 15:28:24 +0900

    1st

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import TopicsList from '@/components/TopicList'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold"> WebDev Topics</h1>
+      <p className="mb-4">Learning example of MongoDB CRUD</p>
+      {/*<TopicsList />*/}
+    </>
+  )
+}
---
+++ components/TopicList.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import RemoveBtn from './RemoveBtn'
+import Link from 'next/link'
+import { HiPencilAlt } from 'react-icons/hi'
+
+const getTopics = async () => {
+  const apiUrl = process.env.API_URL
+  try {
+    const res = await fetch(`${apiUrl}/api/topics`, {
+      cache: 'no-store',
+    })
+    if (!res.ok) {
+      throw new Error('Failed to fetch topics')
+    }
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function TopicsList() {
+  const { topics } = await getTopics()
+  console.log(topics)
+  return (
+    <>
+      {topics.map((topic) => (
+        <div
+          key={topic._id}
+          className="border border-slate-400 p-4 flex justify-between gap-5 bg-slate-100 items-start my-3"
+        >
+          <div>
+            <h2 className="text-wxl font-bold">{topic.title}</h2>
+            <div>{topic.description}</div>
+            <div className="flex gap-4">
+              <p>Created:{topic.createdAt}</p>
+              <p>Updated: {topic.updated}</p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveBtn id={topic._id} />
+            <Link href={'/editTopic/${topic.id}'}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
